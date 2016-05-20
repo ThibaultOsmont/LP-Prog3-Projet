db097d1db0f4ecc8abfdb771da9fdff4d41a8e09
ThibaultOsmont <thyb44@gmail.com>
2016-05-20 12:12:19 +0200

    Structure initiale
    Point de départ du projet, structure des modules Maven OK,  reste les
    web-services et les classes à ajouter

---
+++ src/main/java/fr/iutnantes/Fournisseur/App.java
@@ -0,0 +1,13 @@
+package fr.iutnantes.Fournisseur;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
